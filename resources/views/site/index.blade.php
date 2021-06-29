703af598665c12908a0b8dec51c7793f01802549
Davi <davi-bispo@hotmail.com>
2021-06-29 08:32:30 -0300

    base finalizada

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -15,16 +15,16 @@
 <body>
 
 <div class="jumbotron">
-
-</div>
-<div class="container">
-    <p>Raízes da Infância</p>
+    <div class="container logo">
+        <p>Raízes da Infância</p>
+    </div>
 </div>
 
 <div class="container content">
     <div class="row">
         <div class="col-sm-3">
             <div class="menu-lateral">
+                <p>NAVEGAÇÃO</p>
                 <ul class="nav navbar-expand-sm nav-pills flex-column">
                     <li class="nav-item">
                         <a class="nav-link active" data-toggle="pill" href="#home"><b>HOME</b></a>
@@ -37,33 +37,57 @@
                     </li>
                 </ul>
             </div>
-            <div class="menu-lateral">
-                <h3>Conteúdo 1</h3>
+            <div class="sub-menu-lateral">
+                <h4>Conteúdo 1</h4>
+                <p>Psicologia para Crianças e adolescentes.</p>
             </div>
-            <div class="menu-lateral">
-                <h3>Conteúdo 2</h3>
-            </div>
-            <div class="menu-lateral">
-                <h3>Conteúdo 3</h3>
+            <div class="sub-menu-lateral">
+                <h4>Conteúdo 2</h4>
             </div>
         </div>
-
-        <div class="col-sm-8 main-content">
+        <!-- main content -->
+        <div class="col-sm-9 main-content">
             <!-- Tab panes -->
             <div class="tab-content">
+                <!-- main content home -->
                 <div id="home" class="container tab-pane active"><br>
-                    <h3>HOME</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <!-- carousel -->
+                    <div id="demo" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ul class="carousel-indicators">
+                            <li data-target="#demo" data-slide-to="0" class="active"></li>
+                            <li data-target="#demo" data-slide-to="1"></li>
+                            <li data-target="#demo" data-slide-to="2"></li>
+                        </ul>
+
+                        <!-- The slideshow -->
+                        <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="imgs/carousel/img1.jpg" alt="Los Angeles" width="1100" height="500">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imgs/carousel/img2.jpg" alt="Chicago" width="1100" height="500">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imgs/carousel/img3.jpg" alt="New York" width="1100" height="500">
+                        </div>
+                        </div>
+
+                        <!-- Left and right controls -->
+                        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                            <span class="carousel-control-prev-icon"></span>
+                        </a>
+                        <a class="carousel-control-next" href="#demo" data-slide="next">
+                            <span class="carousel-control-next-icon"></span>
+                        </a>
+                    </div>
                 </div>
+                <!-- main content 1 -->
                 <div id="menu1" class="container tab-pane fade"><br>
                     <h3>Menu 1</h3>
                     <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 </div>
+                <!-- main content 2 -->
                 <div id="menu2" class="container tab-pane fade"><br>
                     <h3>Menu 2</h3>
                     <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
