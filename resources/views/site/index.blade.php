21569c4a9c1eaf0e9244b29064ab1996736dc6ae
Davi <davi-bispo@hotmail.com>
2021-07-02 06:08:48 -0300

    estrutura de layout finalizada

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -11,12 +11,24 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <link rel="stylesheet" href="{{asset('css/app.css')}}" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
 </head>
 <body>
 
 <div class="jumbotron">
-    <div class="container logo">
-        <p>Raízes da Infância</p>
+    <div class="row logo">
+        <div class="col-sm-3">Karla Bispo</div>
+        <div class="col-sm-3">Endereço</div>
+        <div class="col-sm-3">
+            <a class="link-redes" href="" target="_blank"><i class="fab fa-facebook fa-2x"></i></a>
+            <a class="link-redes" href="" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
+        </div>
+        <div class="col-sm-3">
+            <a class="link-telefone" href="https://web.whatsapp.com/send?phone=5582993487949" target="_blank">
+                <i class="fab fa-whatsapp fa-lg"></i> (82) 99348-7949
+            </a>
+        </div>
     </div>
 </div>
 
@@ -38,12 +50,10 @@
                 </ul>
             </div>
             <div class="sub-menu-lateral">
-                <h4>Conteúdo 1</h4>
+                <h5>Conteúdo 1</h5>
                 <p>Psicologia para Crianças e adolescentes.</p>
             </div>
-            <div class="sub-menu-lateral">
-                <h4>Conteúdo 2</h4>
-            </div>
+
         </div>
         <!-- main content -->
         <div class="col-sm-9 main-content">
@@ -84,22 +94,29 @@
                 </div>
                 <!-- main content 1 -->
                 <div id="menu1" class="container tab-pane fade"><br>
-                    <h3>Menu 1</h3>
-                    <p>
-                    Passagem padrão original de Lorem Ipsum, usada desde o século XVI.
-"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    <div class="container">
+                        <h3>Sobre min</h3>
+                        <hr>
+                        <p>
+                            Passagem padrão original de Lorem Ipsum, usada desde o século XVI.
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-Seção 1.10.32 de "de Finibus Bonorum et Malorum", escrita por Cícero em 45 AC
-"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+                            Seção 1.10.32 de "de Finibus Bonorum et Malorum", escrita por Cícero em 45 AC
+                            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
 
-Tradução para o inglês por H. Rackha, feita em 1914
-"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
-                    </p>
+                            Tradução para o inglês por H. Rackha, feita em 1914
+                            "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+                        </p>
+                        <hr>
+                    </div>
                 </div>
                 <!-- main content 2 -->
                 <div id="menu2" class="container tab-pane fade"><br>
-                    <h3>Menu 2</h3>
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                    <div class="container">
+                        <h3>Serviços</h3>
+                        <hr>
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                    </div>
                 </div>
             </div>
         </div>
